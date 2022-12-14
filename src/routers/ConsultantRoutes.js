e00170bc4d345c64c61832c0c588c2f94d4c8a18
DarFelix <xbxb78@gmail.com>
2022-12-14 11:38:09 -0500

    add route rol

--- src/routers/ConsultantRoutes.js
+++ src/routers/ConsultantRoutes.js
@@ -1,31 +1,25 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { HeaderAdmin } from '../components/ui/HeaderAdmin'
+import { HeaderConsultant } from '../components/ui/HeaderConsultant'
 import { HomeView } from '../components/home/HomeView'
 import { UsuarioView } from '../components/usuarios/UsuarioView'
-import { TarjetaView } from '../components/tarjetas/TarjetaView'
 import { CuentaView } from '../components/cuentas/CuentaView'
 import { ProductoView } from '../components/productos/ProductoView'
 import { DepositoView } from '../components/depositos/DepositoView'
-import { TransferenciaView } from '../components/transferencias/TransferenciaView.js'
-import { AbonoView } from '../components/abonos/AbonoView'
 import { UsuarioUpdate } from '../components/usuarios/UsuarioUpdate'
 
-export const AdminRoutes = () => {
+export const ConsultantRoutes = () => {
     return (
         <>
-            <HeaderAdmin />
+            <HeaderConsultant />
 
             <div className="container mt-2">
                 <Switch>
                     <Route exact path='/home' component={HomeView} />
                     <Route exact path='/usuarios' component={UsuarioView} />
-                    <Route exact path='/tarjetas' component={TarjetaView} />
                     <Route exact path='/cuentas' component={CuentaView} />
                     <Route exact path='/productos' component={ProductoView} />
                     <Route exact path='/depositos' component={DepositoView} />
-                    <Route exact path='/transferencias' component={TransferenciaView} />
-                    <Route exact path='/abonos' component={AbonoView} />
                     <Route exact path='/usuarios/:usuarioId' component={UsuarioUpdate} />
                     <Redirect to="/home" />
                 </Switch>
